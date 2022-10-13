24f2066d0c5591824133b83c9d0f3a2ba02e4f76
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-10-13 18:58:17 +0300

    Add button and remove children from container functionality

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,7 +2,7 @@ document.addEventListener('mouseover', (e) => {
     if (e.target.classList.contains('item')) {
         e.target.classList.add('red');
     }
-})
+});
 
 const container = document.querySelector('.container');
 
@@ -14,3 +14,15 @@ for (let i = 0; i < 4; i++) {
         container.appendChild(elem);
     }
 }
+
+const sizeButton = document.querySelector('.size-button');
+
+sizeButton.addEventListener('click', (e) => {
+    let size = parseInt(prompt("What should the side size be? (1 to 100)"));
+    while (size < 1 || size > 100) {
+        size = parseInt(prompt("What should the side size be? (1 to 100)"));
+    }
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+});
