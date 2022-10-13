75e87ad58015ed3cb0075f1d44851aedf2875b34
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-10-13 18:42:38 +0300

    Add pixelated effect on the initial grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,10 +1,16 @@
+document.addEventListener('mouseover', (e) => {
+    if (e.target.classList.contains('item')) {
+        e.target.classList.add('red');
+    }
+})
+
 const container = document.querySelector('.container');
 
 for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
         const elem = document.createElement('div');
-        elem.classList.add('item');
         elem.textContent = i * 4 + j;
+        elem.classList.add('item');
         container.appendChild(elem);
     }
 }
